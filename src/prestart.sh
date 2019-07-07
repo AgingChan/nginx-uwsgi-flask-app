11a8695702aacbbc819e27bd56728d0d299a2f2b
Aging Chan <aging.chan@gmail.com>
2019-07-07 15:20:13 +0800

    schedule uwsgi auto start

--- src/prestart.sh
+++ src/prestart.sh
@@ -2,6 +2,8 @@
 
 echo "Running inside /app/prestart.sh, you could add migrations to this file, e.g.:"
 
+cp /app/supervisor-restart-uwsgi.conf /etc/supervisor/conf.d/
+
 echo "
 #! /usr/bin/env bash
 
